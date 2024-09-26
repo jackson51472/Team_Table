0260f296e61cc7d75db81e4b27a4063d6d78f03e
jnmorais <jsmorais2002@gmail.com>
2024-09-26 08:49:21 -0300

    Criando a tabela

--- TeamTable/src/app/app.module.ts
+++ TeamTable/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Correção aqui
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -13,11 +18,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,  // Certifique-se de importar este módulo
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- TeamTable/src/app/table/table.component.ts
+++ TeamTable/src/app/table/table.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+
+export interface Time {
+  codigo: number;
+  nome: string;
+  anoFundacao: number;
+  estadio: string;
+  serie: string;
+}
+
+const TIMES_DATA: Time[] = [
+  { codigo: 1, nome: 'Flamengo', anoFundacao: 1895, estadio: 'Maracanã', serie: 'A' },
+  { codigo: 2, nome: 'Grêmio', anoFundacao: 1903, estadio: 'Arena do Grêmio', serie: 'A' },
+  { codigo: 3, nome: 'Cruzeiro', anoFundacao: 1921, estadio: 'Mineirão', serie: 'A' },
+  // Adicione mais times aqui
+];
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrls: ['./table.component.scss']
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
+  displayedColumns: string[] = ['codigo', 'nome', 'anoFundacao', 'estadio', 'serie'];
+  dataSource = new MatTableDataSource(TIMES_DATA);
 
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngOnInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
